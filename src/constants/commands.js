1bb28f09cb5770763860f4d8dc338fbbaf64128e
Bella Makhova <rgvder@gmail.com>
2025-05-03 19:19:57 +0300

    feat: add compress, decompress, hash commands

--- src/constants/commands.js
+++ src/constants/commands.js
@@ -7,6 +7,11 @@ import { mkdir } from '../files/mkdir.js';
 import { rn } from '../files/rn.js';
 import { rm } from '../files/rm.js';
 import { os } from '../os/os.js';
+import { cp } from '../files/cp.js';
+import { mv } from '../files/mv.js';
+import { hash } from '../hash/hash.js';
+import { compress } from '../zip/compress.js';
+import { decompress } from '../zip/decompress.js';
 
 export const commands = {
   up: [0, up],
@@ -16,7 +21,12 @@ export const commands = {
   add: [1, add],
   mkdir: [1, mkdir],
   rn: [2, rn],
+  cp: [2, cp],
+  mv: [2, mv],
   rm: [1, rm],
   os: [1, os],
+  hash: [1, hash],
+  compress: [2, compress],
+  decompress: [2, decompress],
 };
 
